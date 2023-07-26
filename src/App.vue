99cb9e345da5d5d2d18af8d1e828a6d88d65c6f1
elanafrica <team@elanafrica.com>
2023-07-26 13:11:35 +0100

    commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <Navbar />
+    <Suspense>
+        <RouterView />
+    </Suspense>
+
+</template>
+
+<script setup>
+import { RouterView } from "vue-router"
+import Navbar from "./components/Navbar.vue";
+</script>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid grid-cols-3 gap-4 mx-20 my-10">
+   <MovieCard 
+    v-for="movie in movies" 
+    :key=movie
+    :movie="movie"
+   />
+  </div>
+</template>
+
+<script setup>
+import MovieCard from './MovieCard.vue';
+const { movies } = defineProps({
+    movies: Array
+})
+</script>
